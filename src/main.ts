434855fda630079049ae09feb98972ab0312b6a4
Samuel <54637954+samodicer@users.noreply.github.com>
2024-08-28 10:31:50 +0200

    setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import './assets/main.css'
+import './style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +11,11 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+})
 
 app.mount('#app')
