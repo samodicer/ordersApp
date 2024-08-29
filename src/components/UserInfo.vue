454ed8a31aad7b804ba910949a153ec8544dd97e
Samuel <54637954+samodicer@users.noreply.github.com>
2024-08-29 17:16:34 +0200

    layout view added

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-if="userStore.user" class="flex items-center justify-between p-2">
+    <router-link class="flex items-center gap-2" to="/profile">
+      <Avatar 
+        class="mr-2" 
+        style="background-color: #ece9fc; 
+        color: #2a1261" 
+        shape="circle" 
+      >
+        <i v-if="!userStore.user.avatar" class="pi pi-question"></i>
+      </Avatar>
+      <span>{{ userStore.user.fullName }}</span>
+    </router-link>
+    <Button
+      icon="pi pi-sign-out"
+      rounded
+      outlined
+      @click="logoutUser"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Button from "primevue/button";
+import Avatar from "primevue/avatar";
+import router from "@/router";
+import { apiLogoutUser } from "@/api/user";
+import { useUserStore } from "@/stores/user";
+
+
+const userStore = useUserStore();
+
+const logoutUser = () => { 
+  apiLogoutUser().then(() => {
+    router.push({path: '/login'})
+  });
+}
+</script>
---
+++ src/views/OrdersView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    ORDERS
+    fsfs
+    dggd
+
+  </div>
+  <div>
+    ORDERS
+    fsfs
+    dggd
+    
+  </div>
+  <p>sddsdsds sddsds</p>
+  <Button>dsds</Button>
+</template>
+
+<script setup lang="ts">
+import Button from 'primevue/button';
+
+
+</script>
+
+<style scoped>
+
+</style>
