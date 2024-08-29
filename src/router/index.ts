73731caa2240c28433154d8279875cf592da6021
Samuel <54637954+samodicer@users.noreply.github.com>
2024-08-29 14:57:51 +0200

    Revert "Merge pull request #2 from samodicer/login_view"
    This reverts commit c8021a063148f06b405fdc7b9f6e34a60d673235, reversing
    changes made to 434855fda630079049ae09feb98972ab0312b6a4.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '../views/LoginView.vue'
-import TheLayout from '@/components/TheLayout.vue'
-
-const routes = [
-  {
-    path: '/',
-    component: TheLayout,
-    name: 'Layout',
-    meta: {
-      requiresAuth: true
-    }
-  },
-  {
-    path: '/login',
-    component: LoginView,
-    name: 'Login'
-  }
-]
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
 })
 
 export default router
--- src/types/user.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-  id: number
-  firstname: string
-  lastname: string
-  fullName: string
-  email: string
-  is_admin: boolean
-  avatar: null
-}
