bba066a9dc943a4e8620955d6b4a0c932e63e58c
Samuel <54637954+samodicer@users.noreply.github.com>
2024-08-31 19:02:45 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import 'primeicons/primeicons.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { useUserStore } from '@/stores/user'
+import { apiGetUser } from './api/user'
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
 import App from './App.vue'
